65c9585b7a59826f458a9e0e5d7ef93d05b8fec3
RihamSamarai <riham.samarai@gmail.com>
2024-11-29 15:29:49 +0100

    added App.js

--- food-donation/src/App.js
+++ food-donation/src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Header from './components/Header';
+import DonationList from './components/DonationList';
+import DonationForm from './components/DonationForm';
+import Footer from './components/Footer';
+import { ContextProvider } from './components/ContextProvider';
+import GlobalStyle from './styles/globalStyles';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ContextProvider>
+      <GlobalStyle />
+      <Header />
+      <DonationForm />
+      <DonationList />
+      <Footer />
+    </ContextProvider>
   );
 }
 
